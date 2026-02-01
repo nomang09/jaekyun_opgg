dfe20f2d0f4ed53cc0dc7e78bf021c1062e4e932
nomang09 <ijkakma@daum.net>
2026-02-01 23:19:52 +0900

    third commit make domain

---
+++ src/main/java/com/staredu/grammar/domain/Score.java
@@ -0,0 +1,33 @@
+package com.staredu.grammar.domain;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Score {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer score;
+    private Date date;
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
